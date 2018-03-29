86845c4d9e17d8eb938e863cd2eaefd1e5adb7fb
Timothy R. Koscik <timkoscik@gmail.com>
2018-03-29 10:54:41 -0500

    Update world.to.mni.R

--- R/world.to.mni.R
+++ R/world.to.mni.R
@@ -5,6 +5,7 @@ world.to.mni <- function(coords, tform.xyz=NULL, nii.file=NULL) {
       stop("Affine transform must be provided or read from nifti file")
     } else {
       tform <- nii.hdr(nii.file, c("srow_x", "srow_y", "srow_z"))
+      tform <- rbind(tform$srow_x, tform$srow_y, tform$srow_z, c(0,0,0,1))
     }
   } else {
     stopifnot(nrow(tform.xyz) == 3)
